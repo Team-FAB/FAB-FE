34d04a07c25dfbb4c1844b00da2a0a80ba03e0f8
jimin <hgm1557@naver.com>
2023-06-05 15:34:14 +0900

    Revert "Revert "add : Modal창 추가""
    This reverts commit 57d51145e285ac6ee5b76c1cc90c22b095efab6d.

--- src/components/PostModal/postModal.tsx
+++ src/components/PostModal/postModal.tsx
@@ -1,22 +1,43 @@
 import React from "react";
-import styles from "./postModal.module.css";
+import { Modal, Badge, Button } from "antd";
+import styles from "./postModal.module.css"
+
 
 interface PostModalProps {
-  post: any; // Roommate post data type
+  post: any; 
   onClose: () => void;
 }
 
 const PostModal: React.FC<PostModalProps> = ({ post, onClose }) => {
     return (
-      <div className={styles.modal}>
-        <div className={styles.modalContent}>
-          <h2>{post.title}</h2>
-          <p>{post.content}</p>
-          {/* Add other post fields as needed */}
-          <button onClick={onClose}>Close</button>
+      <Modal
+        visible={true}
+        onOk={onClose}
+        onCancel={onClose}
+        cancelButtonProps={{ style: { display: 'none' } }}
+        okButtonProps={{ style: { display: 'none' } }}
+      >
+        <Badge className={styles.badgePresent}>모집</Badge>
+        <div className={styles.titleContainer}>
+          <span className={styles.title}>{post.title}</span>
+          <Badge className={styles.save}>찜하기</Badge>
         </div>
-      </div>
+        <div className={styles.content}>{post.content}</div>
+        <div className={styles.ProfileContainer}>
+          <img className={styles.profileImg} src='https://via.placeholder.com/25'></img>
+          <span className={styles.ProfileContent}>{post.nickname} {post.createdDate}</span>
+          <Button className={styles.apply}>신청하기</Button>
+        </div>
+        <div className={styles.line}></div>
+        <div className={styles.cardBadgeContainer}>
+            <Badge className={styles.cardBadgeArea}>{post.region}</Badge>
+            <Badge className={styles.cardBadgePeriod}>{post.period}</Badge>
+            <Badge className={styles.cardBadgePrice}>{post.price}</Badge>
+        </div>
+
+
+      </Modal>
     );
-  };
+};
   
-  export default PostModal;
+export default PostModal;
