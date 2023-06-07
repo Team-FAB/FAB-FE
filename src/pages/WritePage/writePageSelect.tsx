155b6210aa159494612f2a9dce0faee9d9934022
황지민\hgm15 <hgm1557@naver.com>
2023-06-07 21:37:09 +0900

    edit : writePageSelect 수정

--- src/pages/WritePage/writePageSelect.tsx
+++ src/pages/WritePage/writePageSelect.tsx
@@ -12,7 +12,7 @@ const writePageSelect: React.FC<WritePageSelectProps> = ({ form }) => {
   const [searchBoxOpen, setSearchBoxOpen] = useState(false);
   const [selectedArea, setSelectedArea] = useState<string>("지역");
   const [selectedPeriod, setSelectedPeriod] = useState<string>("기간");
-  const [selectedPrice, setSelectedPrice] = useState<number>(0);
+  const [selectedPrice, setSelectedPrice] = useState<String>("보증금");
   const [selectedGender, setSelectedGender] = useState<string>("성별");
 
   const handleRegionChange = (e: RadioChangeEvent) => {
@@ -27,8 +27,8 @@ const writePageSelect: React.FC<WritePageSelectProps> = ({ form }) => {
     form.setFieldsValue({ period });
   };
 
-  const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const price = e.target.value === "" ? 0 : parseInt(e.target.value);
+  const handlePriceChange = (e: RadioChangeEvent) => {
+    const price = e.target.value
     setSelectedPrice(price);
     form.setFieldsValue({ price });
   };
@@ -79,6 +79,14 @@ const writePageSelect: React.FC<WritePageSelectProps> = ({ form }) => {
     { quarter: "1년 이상 ~" },
   ];
 
+  const price = [
+    { deposit: "~ 1,000,000원" },
+    { deposit: "~ 3,000,000원" },
+    { deposit: "~ 5,000,000원" },
+    { deposit: "~ 8,000,000원" },
+    { deposit: "~ 10,000,000원" },
+  ];
+
   const gender = [{ name: "여성" }, { name: "남성" }];
 
   return (
@@ -111,130 +119,124 @@ const writePageSelect: React.FC<WritePageSelectProps> = ({ form }) => {
               <Badge className={styles.cardBadgeGender}>{selectedGender}</Badge>
             </div>
           </div>
-          {searchBoxOpen && (
-            <div className={styles.searchChoiceContainer}>
-              <div className={styles.searchChoiceArea}>
-                <p>지역</p>
-                <div className={styles.areaRadioGroup}>
-                  <Form.Item
-                    name="region"
-                    rules={[
-                      {
-                        required: true,
-                        message: "지역을 선택해 주세요.",
-                      },
-                    ]}
-                  >
-                    <Radio.Group onChange={handleRegionChange}>
-                      {area.map((item, index) => (
-                        <Radio
-                          key={index}
-                          value={item.region}
-                          className={styles.areaRadioBtn}
-                        >
-                          {item.region}
-                        </Radio>
-                      ))}
-                    </Radio.Group>
-                  </Form.Item>
-                </div>
-              </div>
-              <div className={styles.searchChoicePeriod}>
-                <p>기간</p>
+          <div
+            className={
+              searchBoxOpen
+                ? styles.searchChoiceContainer
+                : styles.searchChoiceContainerHide
+            }
+          >
+            <div className={styles.searchChoiceArea}>
+              <p>지역</p>
+              <div className={styles.areaRadioGroup}>
                 <Form.Item
-                  name="period"
+                  name="region"
                   rules={[
                     {
                       required: true,
-                      message: "기간을 선택해 주세요.",
+                      message: "지역을 선택해 주세요.",
                     },
                   ]}
                 >
-                  <Radio.Group
-                    className={styles.periodRadioGroup}
-                    value={selectedPeriod}
-                    onChange={handlePeriodChange}
-                  >
-                    {period.map((item, index) => (
+                  <Radio.Group onChange={handleRegionChange}>
+                    {area.map((item, index) => (
                       <Radio
                         key={index}
-                        value={item.quarter}
-                        className={styles.periodRadioBtn}
+                        value={item.region}
+                        className={styles.areaRadioBtn}
                       >
-                        {item.quarter}
-                      </Radio>
-                    ))}
-                  </Radio.Group>
-                </Form.Item>
-              </div>
-              <div className={styles.searchChoicePrice}>
-                <p>보증금</p>
-                <Form>
-                  <Form.Item
-                    name="price"
-                    rules={[
-                      {
-                        required: true,
-                        message: "필수 입력 사항입니다.",
-                      },
-                      ({ getFieldValue }) => ({
-                        validator(_, value) {
-                          const numericValue = parseInt(value, 10);
-                          if (
-                            !value ||
-                            (numericValue >= 1000000 &&
-                              numericValue <= 10000000)
-                          ) {
-                            return Promise.resolve();
-                          }
-                          return Promise.reject(
-                            new Error(
-                              "가격은 백만원에서 천만원 사이여야 합니다."
-                            )
-                          );
-                        },
-                      }),
-                    ]}
-                  >
-                    <Input
-                      placeholder="가격을 입력해주세요."
-                      className={styles.priceInput}
-                      onChange={handlePriceChange}
-                      maxLength={8}
-                    />
-                  </Form.Item>
-                </Form>
-              </div>
-              <div className={styles.searchChoiceGender}>
-                <p>성별</p>
-                <Form.Item
-                  name="gender"
-                  rules={[
-                    {
-                      required: true,
-                      message: "성별을 선택해 주세요.",
-                    },
-                  ]}
-                >
-                  <Radio.Group
-                    className={styles.genderRadioGroup}
-                    value={selectedGender}
-                    onChange={handleGenderChange}
-                  >
-                    {gender.map((item, index) => (
-                      <Radio
-                        key={index}
-                        value={item.name}
-                        className={styles.genderRadioBtn}
-                      >
-                        {item.name}
+                        {item.region}
                       </Radio>
                     ))}
                   </Radio.Group>
                 </Form.Item>
               </div>
             </div>
-          )}
+            <div className={styles.searchChoicePeriod}>
+              <p>기간</p>
+              <Form.Item
+                name="period"
+                rules={[
+                  {
+                    required: true,
+                    message: "기간을 선택해 주세요.",
+                  },
+                ]}
+              >
+                <Radio.Group
+                  className={styles.periodRadioGroup}
+                  value={selectedPeriod}
+                  onChange={handlePeriodChange}
+                >
+                  {period.map((item, index) => (
+                    <Radio
+                      key={index}
+                      value={item.quarter}
+                      className={styles.periodRadioBtn}
+                    >
+                      {item.quarter}
+                    </Radio>
+                  ))}
+                </Radio.Group>
+              </Form.Item>
+            </div>
+            <div className={styles.searchChoicePrice}>
+              <p>보증금</p>
+              <Form.Item
+                name="price"
+                rules={[
+                  {
+                    required: true,
+                    message: "소유한 보증금을 선택해 주세요.",
+                  },
+                ]}
+              >
+                <Radio.Group
+                  className={styles.priceRadioGroup}
+                  value={selectedPeriod}
+                  onChange={handlePriceChange}
+                >
+                  {price.map((item, index) => (
+                    <Radio
+                      key={index}
+                      value={item.deposit}
+                      className={styles.priceRadioBtn}
+                    >
+                      {item.deposit}
+                    </Radio>
+                  ))}
+                </Radio.Group>
+              </Form.Item>
+            </div>
+            <div className={styles.searchChoiceGender}>
+              <p>성별</p>
+              <Form.Item
+                name="gender"
+                rules={[
+                  {
+                    required: true,
+                    message: "성별을 선택해 주세요.",
+                  },
+                ]}
+              >
+                <Radio.Group
+                  className={styles.genderRadioGroup}
+                  value={selectedGender}
+                  onChange={handleGenderChange}
+                >
+                  {gender.map((item, index) => (
+                    <Radio
+                      key={index}
+                      value={item.name}
+                      className={styles.genderRadioBtn}
+                    >
+                      {item.name}
+                    </Radio>
+                  ))}
+                </Radio.Group>
+              </Form.Item>
+            </div>
+          </div>
         </div>
       </div>
     </>
