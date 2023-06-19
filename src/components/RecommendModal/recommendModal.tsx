19016312e75bb4492f05e7cb5659ce4f00c48671
jimin <hgm1557@naver.com>
2023-06-20 03:03:38 +0900

    edit : 추천 모달 구현 완료

--- src/components/RecommendModal/recommendModal.tsx
+++ src/components/RecommendModal/recommendModal.tsx
@@ -1,105 +1,147 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Button, Checkbox, Input, Modal } from "antd"
 import styles from "./recommendModal.module.css"
 import { RecommendModalProps } from "../../interface/interface"
+import { userArticle } from "../../api"
+import { Post } from "../../interface/interface"
+import PostModal from "../PostModal/postModal"
 
 const RecommendModal: React.FC<RecommendModalProps> = ({
   visible,
   onClose,
+  userProfile,
+  user,
 }) => {
-  const [checked, setChecked] = useState(["female"])
-  const [checkedSmoking, setCheckedSmoking] = useState(["Apple"])
-  const readOnly = true
+  const [checkedGender, setCheckedGender] = useState<string[]>([])
+  const [checkedSmoking, setCheckedSmoking] = useState<string[]>([])
+  const [userArticles, setUserArticles] = useState<Post[]>([])
+  const [selectedArticle, setSelectedArticle] = useState<Post | null>(null)
 
-  const handleChange = () => {
-    if (readOnly) {
-      setChecked(["female"])
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (userProfile) {
+          setCheckedGender([userProfile.gender])
+          setCheckedSmoking([userProfile.issmoke ? "흡연" : "비흡연"])
+          const response = await fetch(`/api/${userArticle}/users/${user.id}`)
+          const data = await response.json()
+          setUserArticles(data.data)
+        }
+      } catch (error) {
+        console.error("Error:", error)
+      }
     }
-  }
-
-  const handleSmokingChange = () => {
-    if (readOnly) {
-      setCheckedSmoking(["Apple"])
-    }
-  }
+    fetchData()
+  }, [userProfile])
 
   const smokingOptions = [
-    { label: "흡연", value: "Apple" },
-    { label: "비흡연", value: "Pear" },
+    { label: "흡연", value: "흡연" },
+    { label: "비흡연", value: "비흡연" },
   ]
 
   const genderOptions = [
-    { label: "여성", value: "female" },
-    { label: "남성", value: "male" },
+    { label: "여성", value: "여성" },
+    { label: "남성", value: "남성" },
   ]
 
-  const lorem =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  const handleArticleClick = async (articleId: string) => {
+    try {
+      const response = await fetch(`/api/articles/${articleId}`, {
+        method: "GET",
+        headers: new Headers({
+          "ngrok-skip-browser-warning": "69420",
+        }),
+      })
+      const data = await response.json()
+      setSelectedArticle(data.data)
+    } catch (error) {
+      console.error("Error:", error)
+    }
+  }
+
 
   return (
-    <Modal
-      centered
-      open={visible}
-      onOk={onClose}
-      onCancel={onClose}
-      closeIcon={null}
-      footer={[
-        <Button key="submit" type="primary" onClick={onClose}>
-          닫기
-        </Button>,
-      ]}
-    >
-      <div className={styles.profileTitle}>
-        <span>OOO님의 프로필</span>
-      </div>
-      <div className={styles.profileBox}>
-        <div className={styles.profileSection}>
-          <span>성별</span>
-          <Checkbox.Group
-            options={genderOptions}
-            value={checked}
-            onChange={handleChange}
-          />
+    <>
+      <Modal
+        centered
+        open={visible}
+        onOk={onClose}
+        onCancel={onClose}
+        closeIcon={null}
+        footer={[
+          <Button key="submit" type="primary" onClick={onClose}>
+            닫기
+          </Button>,
+        ]}
+      >
+        <div className={styles.profileTitle}>
+          <span>
+            <span className={styles.userProfileNickname}>
+              {userProfile?.nickname}
+            </span>{" "}
+            님의 프로필
+          </span>
         </div>
-        <div className={styles.profileSection}>
-          <span>활동시간</span>
-          <Input value={"오전"} style={{ width: 50 }} readOnly />
+        <div className={styles.profileBox}>
+          <div className={styles.profileSection}>
+            <span>성별</span>
+            <Checkbox.Group options={genderOptions} value={checkedGender} />
+          </div>
+          <div className={styles.profileSection}>
+            <span>활동시간</span>
+            <Input
+              value={userProfile?.activityTime}
+              style={{ width: 50 }}
+              readOnly
+            />
+          </div>
+          <div className={styles.profileSection}>
+            <span>흡연</span>
+            <Checkbox.Group options={smokingOptions} value={checkedSmoking} />
+          </div>
+          <div className={styles.profileSection}>
+            <span>MBTI</span>
+            <Input value={userProfile?.mbti} style={{ width: 50 }} readOnly />
+          </div>
+          <div className={styles.profileSection}>
+            <span>지역</span>
+            <Input value={userProfile?.region} style={{ width: 60 }} readOnly />
+          </div>
+          <div className={styles.profileSection}>
+            <span>연령</span>
+            <Input value={userProfile?.myAge} style={{ width: 50 }} readOnly />
+          </div>
+          <div className={styles.profileSection}>
+            <span>본인 소개</span>
+            <Input.TextArea
+              autoSize={{ minRows: 1, maxRows: 5 }}
+              value={userProfile?.detail}
+              style={{
+                maxWidth: 472,
+                overflowWrap: "break-word",
+                wordWrap: "break-word",
+              }}
+              readOnly
+            />
+            <span>작성한 게시글</span>
+            {userArticles.map((article) => (
+              <div className={styles.articleTitle} key={article.id}>
+                <a  onClick={() => handleArticleClick(article.id.toString())}>
+                  {article.title}
+                </a>
+              </div>
+            ))}
+          </div>
         </div>
-        <div className={styles.profileSection}>
-          <span>흡연</span>
-          <Checkbox.Group
-            options={smokingOptions}
-            value={checkedSmoking}
-            onChange={handleSmokingChange}
-          />
-        </div>
-        <div className={styles.profileSection}>
-          <span>MBTI</span>
-          <Input value={"ISTJ"} style={{ width: 50 }} readOnly />
-        </div>
-        <div className={styles.profileSection}>
-          <span>지역</span>
-          <Input value={"마포구"} style={{ width: 60 }} readOnly />
-        </div>
-        <div className={styles.profileSection}>
-          <span>연령</span>
-          <Input value={"20대"} style={{ width: 50 }} readOnly />
-        </div>
-        <div className={styles.profileSection}>
-          <span>본인 소개</span>
-          <Input.TextArea
-            autoSize={{ minRows: 1, maxRows: 5 }}
-            value={lorem}
-            style={{
-              maxWidth: 472,
-              overflowWrap: "break-word",
-              wordWrap: "break-word",
-            }}
-            readOnly
-          />
-        </div>
-      </div>
-    </Modal>
+      </Modal>
+      {selectedArticle && (
+        <PostModal
+          visible={!!selectedArticle}
+          onClose={() => setSelectedArticle(null)}
+          post={selectedArticle}
+        />
+      )}
+    </>
   )
 }
 
